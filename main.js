1cdc52a8bfa589d1c2fbc6ae04d0cc5b90093a84
sinkush <118369433+sinkush@users.noreply.github.com>
2023-12-13 18:51:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+        block_image_object = Img;
+        block_image_object.scaeToWidth(700);
+        block_image_object.scaeToHeight(510);
+        block_image_object.set({
+            top:0,
+            left:0
+        });
+        canvas.add(block_image_object);
+    });
+}
+
+function playSound(){
+    x.play();
+}
